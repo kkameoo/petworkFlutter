1692d72497bfb78c8c7ee8e776b09a7cc891fbe3
Justinbeer <96567017+Justinbeer@users.noreply.github.com>
2025-04-02 11:53:49 +0900

    DESIGN: 채팅 테스트 구현

---
+++ lib/chat_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatefulWidget {
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final TextEditingController _messageController = TextEditingController();
+  List<String> messages = ['안녕하세요!', '반갑습니다!'];
+
+  void _sendMessage() {
+    String message = _messageController.text;
+    if (message.isNotEmpty) {
+      setState(() {
+        messages.add(message);
+        _messageController.clear();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('채팅 테스트')),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Align(
+                    alignment: index % 2 == 0 ? Alignment.centerLeft : Alignment.centerRight,
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: index % 2 == 0 ? Colors.grey[300] : Colors.blue[300],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(messages[index]),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _messageController,
+                    decoration: InputDecoration(labelText: '메시지 입력'),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: _sendMessage,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'login_page.dart';
+import 'chat_page.dart';
 
 void main() {
-  runApp(MyApp());
+  // runApp(MyApp());
+  runApp(MyAppChat());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +14,17 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter 로그인 테스트',
       home: LoginPage(), // 로그인 페이지 연결
+    );
+  }
+}
+
+class MyAppChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter 채팅 테스트',
+      home: ChatPage(), // 채팅 페이지 실행
     );
   }
 }
