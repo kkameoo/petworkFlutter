90c473f839a74d3026206b5b62bd5774925c8d27
Justinbeer <96567017+Justinbeer@users.noreply.github.com>
2025-04-02 12:34:48 +0900

    DESIGN: 메인 기본 틀

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'login_page.dart';
 import 'chat_page.dart';
 
 void main() {
-  // runApp(MyApp());
-  runApp(MyAppChat());
+  runApp(MyApp());
+  // runApp(MyAppChat());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,19 +12,134 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter 로그인 테스트',
-      home: LoginPage(), // 로그인 페이지 연결
+      title: 'Petwork',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/login': (context) => LoginScreen(),
+        '/main': (context) => MainScreen(),
+      },
     );
   }
 }
 
-class MyAppChat extends StatelessWidget {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter 채팅 테스트',
-      home: ChatPage(), // 채팅 페이지 실행
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/login');
+          },
+          child: Text('로그인 하러 가기'),
+        ),
+      ),
+    );
+  }
+}
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter 로그인 테스트',
+//       home: LoginPage(), // 로그인 페이지 연결
+//     );
+//   }
+// }
+
+// class MyAppChat extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter 채팅 테스트',
+//       home: ChatPage(), // 채팅 페이지 실행
+//     );
+//   }
+// }
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _login() {
+    // TODO: API 연동
+    Navigator.pushNamed(context, '/main');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('로그인')),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: '이메일'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: '비밀번호'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _login,
+              child: Text('로그인'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  final List<String> _categories = ['산책', '거래', '고용', '개스타'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Petwork 메인')),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: List.generate(_categories.length, (index) {
+              return ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                },
+                child: Text(_categories[index]),
+              );
+            }),
+          ),
+          Expanded(
+            child: Center(
+              child: Text('${_categories[_selectedIndex]} 리스트'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
